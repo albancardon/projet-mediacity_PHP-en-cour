8890602a639d68910e99640a92224be3b2a695d0
albancardon <alban.cardon@gmail.com>
2021-07-08 09:18:16 +0200

    Ajout Git

--- mediacity-page02-films.php
+++ mediacity-page02-films.php
@@ -37,6 +37,7 @@ include_once 'PHP/header.php'
                 createHtmlFilm($conn,"contenufilm","act","emp3");
                 createHtmlFilm($conn,"contenufilm","act","emp4");
             ?>
+            test
         </section>
     </article>
 
